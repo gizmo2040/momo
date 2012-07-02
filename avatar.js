9495e3a7edc9b07f0d46a3b4b5665127d2ca5d86
gizmo <gizmo.2040@gmail.com>
2012-07-02 13:59:11 -0500

    cambios optimizacion codigo

--- avatar.js
+++ avatar.js
@@ -1,8 +1,9 @@
 
                 
                 
-                    $(document).ready(init);                    
-                    var cont_obj_face = {"lentes":0,"ojos":0,"nariz":0,"bocas":0,"cejas":0};
+                    $(document).ready(init);  
+
+                    var cont_obj_face = {"lentes":0,"ojo":0,"nariz":0,"boca":0,"ceja":0};
                     var cont_obj_body = {"pelo":0,"bup":0,"bdown":0,"shoes":0};
                     var selected = "";
                                         
@@ -10,18 +11,11 @@
                     function init(e){
                           $("#btn_izq").click(btn_izq_cfun);
                           $("#btn_der").click(btn_der_cfun);
-                          $("#lentes_btn").click(btn_lentes);
-                          $("#ojo_btn").click(btn_ojo);
-                          $("#ceja_btn").click(btn_ceja);
-                          $("#boca_btn").click(btn_boca);
-                          $("#nariz_btn").click(btn_nariz);
-                          $("#pelo_btn").click(btn_pelo);
-                          $("#bup_btn").click(btn_bup);
-                          $("#bdown_btn").click(btn_bdown);
-                          $("#shoes_btn").click(btn_shoes);
+                          $(".btn_select").click(btn_select_click);
                     }
                     
-                    function btn_izq_cfun(e){                        
+                    function btn_izq_cfun(e){  
+                                          
                         if(cont_obj_face.hasOwnProperty(selected)){
                           var pixels = 50;    
                           var cont_obj = cont_obj_face; 
@@ -51,56 +45,11 @@
                         }
                     }                    
                     
-                    function btn_lentes(e){
-                        selected = "lentes";
-                        console.log("lentes");
-                        $("#imprimir").html(selected);
-                    }
-                    
-                    function btn_ojo(e){
-                        selected = "ojos";
-                        console.log("ojos");
-                        $("#imprimir").html(selected);
-                    }                    
-                    
-                    function btn_ceja(e){
-                        selected = "cejas";
-                        console.log("cejas");
+                    function btn_select_click(e){
+                        selected = $(e.currentTarget).attr("id").split("_")[0];
                         $("#imprimir").html(selected);
                     }
                                         
-                    function btn_boca(e){
-                        selected = "bocas";
-                        console.log("bocas");
-                        $("#imprimir").html(selected);
-                    }
-                                        
-                    function btn_nariz(e){ 
-                        selected = "nariz";
-                        console.log("nariz");
-                        $("#imprimir").html(selected);
-                    }
-                    function btn_pelo(e){
-                        selected = "pelo";
-                        console.log("pelo");
-                        $("#imprimir").html(selected);
-                     }
-                    function btn_bup(e){
-                        selected = "bup";
-                        console.log("bup");
-                        $("#imprimir").html(selected);
-                     }
-                     function btn_bdown(e){
-                        selected = "bdown";
-                        console.log("bdown");
-                        $("#imprimir").html(selected);
-                     }
-                     function btn_shoes(e){
-                        selected = "shoes";
-                        console.log("shoes");
-                        $("#imprimir").html(selected);
-                     }
-                    
                     function updateAvatar(cont_obj,pixels){
                         $("#"+selected).css("background", "url('img/"+selected + ".png') no-repeat -"+ (cont_obj[selected] * pixels) +"px 0px");
                     }               
