5c10e588e6ea119e18833e943c7834304d777d7e
David Cantu <dvantul23@gmail.com>
2012-07-03 16:13:21 -0500

    cambios en html y js avatar

--- avatar.js
+++ avatar.js
@@ -1,3 +1,76 @@
+var configObj = {
+	"base":{
+		"file":'img/ninabase.png'
+	},
+	"ojos":{
+		"file": 'img/ojo.png',
+		"width": 50,
+		"height": 50,                
+		"selected": 1,
+                "max":4
+	},
+	"cejas":{
+                "file": 'img/ceja.png',
+		"width": 50,
+                "height": 50,
+                "selected": 1,
+                "max":4
+    },
+    "nariz":{
+                "file": 'img/nariz.png',
+		"width": 50,
+                "height": 50,
+		"selected": 1,
+                "max":4
+    },
+    "bocas":{
+                "file": 'img/boca.png',
+		"width": 50,
+                "height": 50,
+		"selected": 1,
+                "max":4
+    },
+    "pelo":{
+                "file": 'img/pelo.png',
+		"width": 100,
+                "height": 323,
+		"selected": 1,
+                "top": -5,
+                "left": -22,
+                "max":4
+    },
+    "bup":{
+                "file": 'img/bup.png',
+		"width": 100,
+                "height": 323,
+		"selected": 1,
+                "top": -5,
+                "left": -22,
+                "max":4
+    },
+    "bdown":{
+                "file": 'img/bdown.png',
+		"width": 100,
+                "height": 323,
+		"selected": 1,
+                "top": -5,
+                "left": -22,
+                "max":4
+    },
+    "shoes":{
+                "file": 'img/shoes.png',
+		"width": 100,
+                "height": 323,
+		"selected": 1,
+                "top": -5,
+                "left": -22,
+                "max":4
+    }
+}
+
+
+
+
 
                 
                 
@@ -5,53 +78,65 @@
 
                     var cont_obj_face = {"lentes":0,"ojo":0,"nariz":0,"boca":0,"ceja":0};
                     var cont_obj_body = {"pelo":0,"bup":0,"bdown":0,"shoes":0};
-                    var selected = "";
-                                        
+                    var selected = "";  
                     
                     function init(e){
                           $("#btn_izq").click(btn_izq_cfun);
                           $("#btn_der").click(btn_der_cfun);
+                          crearDivsAvatar();
+                          crearBtns();
                           $(".btn_select").click(btn_select_click);
                     }
+                    function crearBtns(){
+                        for(key in configObj){
+                              var tmpDiv = $("<div/>",{
+                                    id: key +"_btn",
+                                    class:"btn_select" 
+                                    
+                              });
+                              tmpDiv.html(key);                            
+                              tmpDiv.appendTo("#botones");
+                              console.log("---->",key,configObj[key])                              
+                        }
+                    }
+                    
+                    function crearDivsAvatar(){
+                        for(key in configObj){
+                              var tmpDiv = $("<div/>",{
+                                    id: "avatar_part_" + key
+                                    
+                              });
+                              tmpDiv.css("position","absolute")
+                              configObj[key]["width"] && tmpDiv.css("width",configObj[key]["width"]);
+                              configObj[key]["height"] && tmpDiv.css("height",configObj[key]["height"]);
+                              configObj[key]["top"] && tmpDiv.css("top",configObj[key]["top"]);
+                              configObj[key]["left"] && tmpDiv.css("left",configObj[key]["left"]);
+                              configObj[key]["file"] && tmpDiv.css("background", "url('"+ configObj[key]["file"] + "') no-repeat 0px 0px");
+                              tmpDiv.appendTo("#avatar_face_wrap");
+                              console.log("---->",key,configObj[key])                              
+                        }
+                    }
+                    
                     
                     function btn_izq_cfun(e){  
-                                          
-                        if(cont_obj_face.hasOwnProperty(selected)){
-                          var pixels = 50;    
-                          var cont_obj = cont_obj_face; 
-                        }else{
-                          var pixels = 100;    
-                          var cont_obj = cont_obj_body;                             
-                        }
-                        
-                        if(cont_obj[selected]>=0){
-                        --cont_obj[selected];
-                        console.log("izquierda");
-                        updateAvatar(cont_obj,pixels);
-                        }
+                        if(--configObj[selected].selected <=0) configObj[selected].selected = configObj[selected].max;
+                        updateAvatar();
                     }                
                     function btn_der_cfun(e){
-                        if(cont_obj_face.hasOwnProperty(selected)){
-                          var pixels = 50;    
-                          var cont_obj = cont_obj_face; 
-                        }else{
-                          var pixels = 100;    
-                          var cont_obj = cont_obj_body;                             
-                        }
-                        if(cont_obj[selected] <3){
-                        ++cont_obj[selected];
-                        console.log("derecha")
-                        updateAvatar(cont_obj,pixels);
-                        }
+                        if(++configObj[selected].selected > (configObj[selected].max -1)) configObj[selected].selected = 0;
+                        updateAvatar();
                     }                    
                     
                     function btn_select_click(e){
                         selected = $(e.currentTarget).attr("id").split("_")[0];
+                        $(".green_select").removeClass("green_select");
+                        $(e.currentTarget).addClass("green_select");
                         $("#imprimir").html(selected);
+                        console.log(selected);
                     }
                                         
                     function updateAvatar(cont_obj,pixels){
-                        $("#"+selected).css("background", "url('img/"+selected + ".png') no-repeat -"+ (cont_obj[selected] * pixels) +"px 0px");
+                        $("#avatar_part_"+selected).css("background", "url('"+ configObj[selected]["file"] + "') no-repeat -"+ (configObj[selected]["width"] * configObj[selected]["selected"]) + "px 0px");
                     }               
                     
                
